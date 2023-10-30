aaa6e01fd4d17cd505cf0e7efcf272b8e73eda29
Daniel Armstrong <59901031+DArmstrong87@users.noreply.github.com>
2023-10-29 21:29:45 -0500

    added mui and splash screen (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
 import reportWebVitals from './reportWebVitals';
+import Splash from './components/Splash';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home title="Hello" disabled={false} />
+    <Splash />
   </React.StrictMode>
 );
 
