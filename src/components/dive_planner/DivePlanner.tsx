c40259caac23ddcb02d7b922e08d8917d2e489df
Danny Armstrong <lenzflare@gmail.com>
2023-11-22 18:43:39 -0600

    almost complete OG dive planner

--- src/components/dive_planner/DivePlanner.tsx
+++ src/components/dive_planner/DivePlanner.tsx
@@ -7,54 +7,80 @@ export interface DiveFormState {
     time: number;
     surfaceInterval: number;
 }
+
+export interface DivePlan {
+    depth: number,
+    startingPressureGroup: string,
+    startingPressureIndex: number,
+    postDivePressureGroup: string,
+    postDivePressureIndex: number,
+    safetyStopRequired: boolean,
+    minToNDL: number,
+    surfaceInterval: number,
+    actualBottomTime: number,
+    residualNitrogenTime: number,
+    totalBottomTime: number,
+}
+
 const initialDiveFormState = {
     depth: 0,
     time: 0,
     surfaceInterval: 0
 }
 
-export interface DivePlan {
-    depth: number,
-    time: number,
-    pressureGroup: string,
-    pressureGroupIndex: number,
-    safetyStopRequired: boolean,
-    minToNDL: number
-}
-
 function DivePlanner() {
 
     const [currentDives, setCurrentDives] = useState<DivePlan[]>([])
-    const [formState, setFormState] = useState<DiveFormState>(initialDiveFormState)
+    const [diveFormState, setFormState] = useState<DiveFormState>(initialDiveFormState)
+    const [prevDivePG, setPrevDivePG] = useState<string>("")
 
     useEffect(() => {
         console.log('Listing dives in current plan')
     }, [currentDives])
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // Prevent NaN
         let value = parseInt(e.target.value)
         if (isNaN(value)) {
             value = 0
         }
+        // Set form state
         setFormState({
-            ...formState,
+            ...diveFormState,
             [e.target.name]: value,
         });
     };
 
+
     const handleSubmit = useCallback(() => {
         const previousDive = currentDives.length > 0 ? currentDives[currentDives.length - 1] : null
-        const plannedDive = planDive(formState, previousDive)
+        const plannedDive = planDive(diveFormState, previousDive)
         const dive = {
-            "depth": formState.depth,
-            "time": formState.time,
-            "pressureGroup": plannedDive.group,
-            "pressureGroupIndex": plannedDive.index,
+            "depth": diveFormState.depth,
+            "startingPressureGroup": plannedDive.startingPressureGroup,
+            "startingPressureIndex": plannedDive.startingPressureIndex,
+            "postDivePressureGroup": plannedDive.postDivePressureGroup,
+            "postDivePressureIndex": plannedDive.postDivePressureIndex,
             "safetyStopRequired": plannedDive.safetyStopRequired,
-            "minToNDL": plannedDive.minToNDL
+            "minToNDL": plannedDive.minToNDL,
+            "surfaceInterval": diveFormState.surfaceInterval,
+            "actualBottomTime": diveFormState.time,
+            "residualNitrogenTime": plannedDive.residualNitrogenTime,
+            "totalBottomTime": plannedDive.totalBottomTime
         }
+        setPrevDivePG(plannedDive.postDivePressureGroup)
+        let resetFormState = { ...initialDiveFormState }
+        if (['Y', 'Z'].includes(plannedDive.postDivePressureGroup)) {
+            resetFormState['surfaceInterval'] = 180
+        }
+        setFormState(resetFormState)
         setCurrentDives([...currentDives, dive])
-    }, [currentDives, formState])
+    }, [currentDives, diveFormState])
+
+    const handleReset = useCallback(() => {
+        setCurrentDives([]);
+        setFormState(initialDiveFormState);
+    }, [])
 
     return (<>
         <h1>Dive Planner</h1>
@@ -73,16 +99,67 @@ function DivePlanner() {
                 currentDives.map((dive, index) =>
                     <React.Fragment key={`diveFrag-${index}`}>
                         <section key={`dive-${index}`}>
+                            {/* Subsequent dives will display surface interval and starting pressure group */}
+                            {index !== 0 ?
+                                <>
+                                    <p key={`diveSI-${index}`}>Surface Interval: {dive.surfaceInterval} minutes</p>
+                                    <p key={`diveStartPG-${index}`}>Starting Pressure Group: {dive.startingPressureGroup}</p>
+                                </>
+                                : ""
+                            }
                             <p key={`diveDepth-${index}`}>Depth: {dive.depth}</p>
-                            <p key={`diveTime-${index}`}>Time: {dive.time}</p>
-                            <p key={`divePG-${index}`}>Pressure Group: {dive.pressureGroup}</p>
-                            <p key={`diveSSRequired-${index}`}>Safety Stop Required: {String(dive.safetyStopRequired)}</p>
-                            <p key={`diveMinToNDL-${index}`}>
-                                {dive.minToNDL < 0 ? `No Decompression limit exceeded by ${Math.abs(dive.minToNDL)} minutes`
-                                : dive.minToNDL > 0 ? `Minutes to No Decompression Limit: ${Math.abs(dive.minToNDL)}`
-                                : `No Decompression level met at ${Math.abs(dive.minToNDL)} minutes`
+                            {/* Subsequent dives will show addition of ABT and RNT for TBT */}
+                            {
+                                index !== 0 ?
+                                    <React.Fragment key={`diveTimesFrag-${index}`}>
+                                        <p key={`diveABT-${index}`}>Actual Bottom Time: {dive.actualBottomTime} minutes</p>
+                                        <p key={`diveRNT-${index}`}>+ Residual nitrogen time: {dive.residualNitrogenTime} minutes</p>
+                                        <p key={`diveTBT-${index}`}>= Total bottom time: {dive.totalBottomTime} minutes</p>
+                                    </React.Fragment>
+                                    :
+                                    <p key={`diveTime-${index}`}>Total Bottom Time: {dive.totalBottomTime}</p>
+                            }
+
+                            <p key={`divePG-${index}`}>Pressure Group: {dive.postDivePressureGroup}</p>
+                            <p key={`diveSSRequired-${index}`}>
+                                Safety Stop Required:
+                                {
+                                    dive.safetyStopRequired && dive.minToNDL < 0 ? " 3 minutes"
+                                        : dive.safetyStopRequired && dive.minToNDL <= 5 && dive.minToNDL >= 0 ? " 8 minutes"
+                                            : dive.safetyStopRequired && dive.minToNDL > 5 ? " 15 minutes"
+                                                : " No"
                                 }
                             </p>
+                            {/* Decompression Limit */}
+                            <p key={`diveMinToNDL-${index}`}>
+                                {dive.minToNDL > 0 ?
+                                    <b>
+                                        ❗ No Decompression limit exceeded by {dive.minToNDL} minutes.
+                                        This dive is highly discouraged.
+                                        This dive requires {dive.minToNDL <= 5 ? "an 8" : "a 15"} minute decompression stop (air supply permitting).
+                                        The diver must remain out of the water for {dive.minToNDL <= 5 ? "6" : "24"} hours before the next dive.
+                                    </b>
+                                    : dive.minToNDL < 0 ? `Minutes to No Decompression Limit: ${Math.abs(dive.minToNDL)}`
+                                        :
+                                        <b>
+                                            ❗ No Decompression limit met at {dive.totalBottomTime} minutes.
+                                            This dive is highly discouraged.
+                                            This dive requires an 8 minute decompression stop (air supply permitting).
+                                            The diver must remain out of the water for 6 hours before the next dive.
+                                        </b>
+                                }
+                            </p>
+                            {/* Pre-flight surface interval minimums */}
+                            <p key={`diveFlightSI-${index}`}>
+                                Pre-flight surface interval:
+                                {index === 0 && dive.minToNDL < 0 ?
+                                    " 12 hour minimum. If this dive follows multi-day dives, the minimum pre-flight surface interval is 18 hours."
+                                    :
+                                    index === 0 && dive.minToNDL >= 0 ? " 18 hour minimum"
+                                        :
+                                        " 18 hour minimum"}
+                            </p>
+                            <hr />
                         </section>
                     </React.Fragment>
                 )
@@ -90,25 +167,35 @@ function DivePlanner() {
             }
 
             <section>
-                <fieldset>
-                    <label key={'depthLabel'} htmlFor="depth">Depth</label>
-                    <input key={'depthInput'} name="depth" type="text" value={formState.depth} required onChange={handleInputChange} />
-                </fieldset>
-
-                <fieldset>
-                    <label key={'timeLabel'} htmlFor="time">Time</label>
-                    <input key={'timeInput'} name="time" type="text" value={formState.time} required onChange={handleInputChange} />
-                </fieldset>
-
                 {currentDives.length > 0 ?
-                    <fieldset>
-                        <label key={'surfaceIntervalLabel'} htmlFor="surfaceInterval">Surface Interval (mins)</label>
-                        <input key={'surfaceIntervalInput'} name="surfaceInterval" type="number" value={formState.surfaceInterval} required onChange={handleInputChange} />
-                    </fieldset>
+                    <>
+                        {prevDivePG === 'Y' || prevDivePG === 'Z' ?
+                            `Previous dive's pressure group is "${prevDivePG}". The minimum suggested surface interval is 3 hours (180 minutes)`
+                            :
+                            ""}
+                        <fieldset>
+                            <label key={'surfaceIntervalLabel'} htmlFor="inputSurfaceInterval">Surface Interval (mins)</label>
+                            <input key={'surfaceIntervalInput'} id="inputSurfaceInterval" name="surfaceInterval" type="number"
+                                min={prevDivePG === 'Y' || prevDivePG === 'Z' ? 180 : ""} value={diveFormState.surfaceInterval}
+                                required onChange={handleInputChange} />
+                        </fieldset>
+                    </>
                     : ""
                 }
 
+                <fieldset>
+                    <label key={'depthLabel'} htmlFor="inputDepth">Depth</label>
+                    <input key={'depthInput'} id="inputDepth" name="depth" type="text" value={diveFormState.depth}
+                        required onChange={handleInputChange} />
+                </fieldset>
+
+                <fieldset>
+                    <label key={'timeLabel'} htmlFor="inputTime">Time (mins)</label>
+                    <input key={'timeInput'} id="inputTime" name="time" type="text" value={diveFormState.time} required onChange={handleInputChange} />
+                </fieldset>
+
                 <button onClick={handleSubmit}>Dive</button>
+                <button onClick={handleReset}>Reset</button>
 
             </section>
 
--- src/components/dive_planner/PlanDive.tsx
+++ src/components/dive_planner/PlanDive.tsx
@@ -1,5 +1,5 @@
 import { DiveFormState, DivePlan } from './DivePlanner';
-import { DEPTHS, DEPTH_TIMES, SAFETY_STOP_DECO_LIMITS } from './DiveTables';
+import { DEPTHS, DEPTH_TIMES, SAFETY_STOP_DECO_LIMITS, SI_TIMES_PG_MATRIX, RNT_NDL_MATRIX } from './DiveTables';
 
 
 /* 1. Get Depth index to get list of times */
@@ -35,41 +35,98 @@ export const planDive = (data: DiveFormState, previousDive: DivePlan | null) => 
         Convert timeIndex to letter which is the pressure group
     */
     const depth = data.depth
-    const time = data.time
+    const actualBottomTime = data.time
+    let totalBottomTime = actualBottomTime
 
-    // Depth and time indexes
+    // Depth index
     const depthIndex = getDepthIndex(depth)
-    const timeIndex = getTimeIndex(depthIndex, time)
+    let startingPressureGroupIndex = 0
 
-    console.log("Previous dive", previousDive)
+    let residualNitrogenTime = 0
+    // Handle secondary dives
+    if (previousDive) {
+        // Get starting pressure group after surface interval
+        const prevDivePressureGroup = previousDive.postDivePressureIndex
+        const surfaceIntervalTime = data.surfaceInterval
+        const PGafterSurfaceInterval = getPgAfterSurfaceInterval(prevDivePressureGroup, surfaceIntervalTime)
+        startingPressureGroupIndex = PGafterSurfaceInterval
+
+        // Residual nitrogen time- add to actual bottom time to get total bottom time
+        const RNT_NDL = getResidualNitrogenTime(depth, actualBottomTime, PGafterSurfaceInterval)
+        residualNitrogenTime = RNT_NDL[0] 
+        totalBottomTime = residualNitrogenTime + actualBottomTime
+    }
+
+    const timeIndex = getTimeIndex(depthIndex, totalBottomTime)
 
     // Safety Stop, Deco Limit
     const minSafetyStopTime_decoLimit = SAFETY_STOP_DECO_LIMITS[depthIndex]
-    const safetyStopRequired = time >= minSafetyStopTime_decoLimit[0]
+    const safetyStopRequired = totalBottomTime >= minSafetyStopTime_decoLimit[0]
     const decoLimit = minSafetyStopTime_decoLimit[1]
+
+    // Matches or exceeds deco limit?
+    const minToNDL = totalBottomTime - decoLimit
+    const exceedsDecoLimit = totalBottomTime > decoLimit
+    const matchesDecoLimit = totalBottomTime === decoLimit
     console.log("Safety stop required?", safetyStopRequired)
-    const minToNDL = decoLimit - time
-    const exceedsDecoLimit = time > decoLimit
-    const matchesDecoLimit = time === decoLimit
     console.log("Minutes to deco limit: ", minToNDL)
 
-    if (exceedsDecoLimit){
+    if (exceedsDecoLimit) {
         console.log(`No Deco Limit exceeded by ${Math.abs(minToNDL)} minutes`)
     }
-    if (matchesDecoLimit){
+    if (matchesDecoLimit) {
         console.log(`Dive time meets No Deco Limit.`)
     }
 
     // timeIndex points to the correct letter for pressure group
-    const pressureGroup = String.fromCharCode('A'.charCodeAt(0) + timeIndex)
-    console.log(`Dive to ${depth} feet for ${time} minutes.`)
-    console.log("Pressure Group After Dive: ", pressureGroup, "\n")
+    const postDivePressureGroup = String.fromCharCode('A'.charCodeAt(0) + timeIndex)
+    console.log(`Dive to ${depth} feet for ${actualBottomTime} minutes.`)
+    console.log("Pressure Group After Dive: ", postDivePressureGroup, "\n")
 
     const divePlan = {
-        "group": pressureGroup,
-        "index": timeIndex,
+        "postDivePressureGroup": postDivePressureGroup,
+        "postDivePressureIndex": timeIndex,
+        "startingPressureGroup": String.fromCharCode('A'.charCodeAt(0) + startingPressureGroupIndex),
+        "startingPressureIndex": startingPressureGroupIndex,
         "safetyStopRequired": safetyStopRequired,
         "minToNDL": minToNDL,
+        "residualNitrogenTime": residualNitrogenTime,
+        "totalBottomTime": totalBottomTime
     }
     return divePlan
+}
+
+const getPgAfterSurfaceInterval = (startGroupIndex: number, surfaceIntervalTime: number) => {
+    const startPressureGroup = String.fromCharCode('A'.charCodeAt(0) + startGroupIndex)
+    console.log(`Pressure Group after previous dive: ${startPressureGroup}`)
+    console.log(`Surface Interval Time: ${surfaceIntervalTime} minutes`)
+
+    const times = SI_TIMES_PG_MATRIX[startGroupIndex]
+    const timeIndex = times.findIndex(arr => surfaceIntervalTime >= arr[0] && surfaceIntervalTime <= arr[1])
+
+    // const PGafterSurfaceInterval = timeIndex !== -1 ? String.fromCharCode('A'.charCodeAt(0) + timeIndex) : 'A'
+    return timeIndex !== -1 ? timeIndex : 0;
+
+}
+
+const getResidualNitrogenTime = (depth: number, actualBottomTime: number, pgIndex: number) => {
+    const startPressureGroup = String.fromCharCode('A'.charCodeAt(0) + pgIndex)
+    console.log("Starting group", startPressureGroup)
+
+    // Use depth to get [residual nitrogen time, no deco limit] from matrix
+    const depthIndex = getDepthIndex(depth)
+    const times = RNT_NDL_MATRIX[depthIndex]
+    return times[pgIndex]
+    // const totalBottomTime = actualBottomTime + residualNitrogenTime
+
+    // console.log("Actual Bottom Time", actualBottomTime)
+    // console.log("Residual Nitrogen Time", residualNitrogenTime)
+    // console.log("Total Bottom time", totalBottomTime)
+    // console.log("No Deco Limit", noDecoLimit)
+
+    // if (totalBottomTime > noDecoLimit) {
+    //     console.log("EXCEEDS DECO LIMITS")
+    // } else {
+    //     console.log("Total Bottom time within deco limits")
+    // }
 }
